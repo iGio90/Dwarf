9abe641b24641276e7af6d4fa313f0d18ac0e26a
PinkiePieStyle <svenw1980@gmail.com>
2019-06-08 07:22:41 +0200

    prep stuff

---
+++ lib/types/cpu_register.py
@@ -0,0 +1,26 @@
+"""
+Dwarf - Copyright (C) 2019 Giovanni Rocca (iGio90)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+class CpuRegister():
+    __slots__ = ['name', 'description', 'value']
+
+    def __init__(self, name, value, description=None):
+        self.name = name
+        self.value = value
+        self.description = 'Register'
+        if description:
+            self.description = description
