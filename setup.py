d218700cfeb47c077e01e3af1b88eae1a500442e
PinkiePieStyle <svenw1980@gmail.com>
2019-08-02 15:23:46 +0200

    prerelease++

--- setup.py
+++ setup.py
@@ -5,29 +5,22 @@ from dwarf.dwarf import __version__
 setup(
 
     # Package info
-    name = 'dwarf',
-    version = __version__,
-    packages = find_packages(),
+    name='dwarf',
+    version=__version__,
+    packages=find_packages(),
     python_requires='>=3',
     package_data={
-        '': ['assets/*'],
-        '': ['assets/icons/*'],
-        '': ['lib/core.js']
+        '': ['assets/*', 'assets/icons/*', 'lib/core.js']
     },
     zip_safe=False,
     include_package_data=True,
     # Dependencies
-    install_requires = [
-        'capstone>=4.0.1',
-        'requests>=2.18.4',
-        'frida>=12.6.11',
-        'PyQt5>=5.11.3',
-        'pyperclip>=1.7.0'
+    install_requires=[
+        'capstone==4.0.1',
+        'requests==2.22.0',
+        'frida==12.6.11',
+        'PyQt5==5.11.3',
+        'pyperclip==1.7.0'
     ],
     # Script info
-    entry_points = {
-        'console_scripts': [
-            'dwarf = dwarf.dwarf:main'
-        ]
-    }
-)
+    entry_points={'console_scripts': ['dwarf = dwarf.dwarf:main']})
