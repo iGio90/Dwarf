339c8e3f4a98f9ad767da3952ad2d4ef9736c8e9
Giovanni Rocca <giovanni.rocca.90@gmail.com>
2019-08-25 14:20:39 +0200

    fix breakpoints from module name

--- dwarf/ui/app.py
+++ dwarf/ui/app.py
@@ -942,7 +942,7 @@ class AppWindow(QMainWindow):
 
     def _on_addmodule_breakpoint(self, data):
         ptr, name = data
-        self.dwarf.breakpoint_native(ptr, own_input=name)
+        self.dwarf.breakpoint_native(ptr, input_=name)
 
     def on_tid_resumed(self, tid):
         if self.dwarf:
